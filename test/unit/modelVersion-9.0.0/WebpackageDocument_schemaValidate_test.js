144640b470b06579ba0f67e6939996a18a88c61d
Philipp Wagner <philipp.wagner@incowia.com>
2016-08-05 15:43:33 +0200

    PLAT-552: create model version 9 schema (WIP)

---
+++ test/unit/modelVersion-9.0.0/WebpackageDocument_schemaValidate_test.js
@@ -0,0 +1,38 @@
+/*global require,describe,beforeEach,it,assert*/
+describe('WebpackageDocument Schema Validation (modelVersion 9.0.0)', function () {
+  var WebpackageDocument;
+  var fs;
+  var path;
+  describe('validate', function () {
+    beforeEach(function () {
+      fs = require('fs');
+      path = require('path');
+      WebpackageDocument = require('../../../lib/WebpackageDocument');
+    });
+
+    describe('validation of repeatedValues', function () {
+      var manifestWebpackageString;
+      beforeEach(function () {
+        var pathName = path.resolve(__dirname, '../../resource/modelVersion-9.0.0/manifest.webpackage');
+        manifestWebpackageString = fs.readFileSync(pathName, 'utf8');
+      });
+
+      it('should be validated successfully', function () {
+        var manifestWebpackageObject = JSON.parse(manifestWebpackageString);
+        var webpackageDocument = new WebpackageDocument(manifestWebpackageObject);
+        var validationState;
+        var onSuccess = function () {
+          validationState = true;
+        };
+        var onUnsupportedModelVersionError = function (error) {
+          assert.fail(error, null, 'No error not expected.');
+        };
+        var onValidationError = function (errors) {
+          assert.fail(errors, null, 'No error not expected.');
+        };
+        webpackageDocument.validate(onSuccess, onUnsupportedModelVersionError, onValidationError);
+        validationState.should.be.equal(true);
+      });
+    });
+  });
+});
